66a95026b935a113860bfa345e6be98ed2dd6e1a
Saikrishna <saikrishna.kn@gmail.com>
2020-03-09 17:03:18 +0530

    Change in Test Code

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -38,7 +38,7 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingComment() {
     String someone = "Hello World, crazy here";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
